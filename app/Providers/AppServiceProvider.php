1a7fbcfabf81b6f1ec1d06a1fbf3ad1bbb76634d
Vincent Agbuya <23sc4114_ms@psu.edu.ph>
2026-05-22 22:42:08 +0800

    Avoid boot-time DB schema access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -21,7 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
         Paginator::useBootstrap();
     }
 }
