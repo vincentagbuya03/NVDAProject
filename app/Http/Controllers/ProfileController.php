7b7d2e4f2e2219590c3cf695d856c6112b4a8825
Vincent Agbuya <23sc4114_ms@psu.edu.ph>
2026-05-22 23:47:16 +0800

    PUSHED

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
